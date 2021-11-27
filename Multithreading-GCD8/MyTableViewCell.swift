a81c93de885d7e7ac2520045021fe4e0ea5dce10
Rus <de.mr44@gmail.com>
2021-11-27 23:33:03 +0500

    Added the image setting up in the cells

--- Multithreading-GCD8/MyTableViewCell.swift
+++ Multithreading-GCD8/MyTableViewCell.swift
@@ -10,15 +10,29 @@ import UIKit
 class MyTableViewCell: UITableViewCell {
 
     @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myActivityIndicatorView: UIActivityIndicatorView!
     
     var imageURL: URL? {
         didSet {
             myImageView.image = nil
-            updateUI()
+            setUpImage()
         }
     }
     
-    private func updateUI() {
-        
+    private func setUpImage() {
+        if let imageURL = imageURL {
+            myActivityIndicatorView.startAnimating()
+            DispatchQueue.global(qos: .utility).async {
+                if let data = try? Data(contentsOf: imageURL) {
+                    DispatchQueue.main.async {
+                        // an additional condition for correct image setting up
+                        if imageURL == self.imageURL {
+                            self.myImageView.image = UIImage(data: data)
+                            self.myActivityIndicatorView.stopAnimating()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- Multithreading-GCD8/ViewController.swift
+++ Multithreading-GCD8/ViewController.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 
+// practicing with the image downloading in the tables
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var myTableView: UITableView!
     let imageURLs: [String] = [
-        "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
-        "https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
-        "https://images.unsplash.com/photo-1439853949127-fa647821eba0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-        "https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
+        "https://images.unsplash.com/photo-1519181245277-cffeb31da2e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+        "https://images.unsplash.com/photo-1536147116438-62679a5e01f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "https://images.unsplash.com/photo-1468971050039-be99497410af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3132&q=80",
+        "https://images.unsplash.com/photo-1505356822725-08ad25f3ffe4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "https://images.unsplash.com/photo-1511974212900-b42a18e19eb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "https://images.unsplash.com/photo-1605003179269-c446bb939f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "https://images.unsplash.com/photo-1583339522870-0d9f28cef33f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4140&q=80",
+        "https://images.unsplash.com/photo-1554050857-c84a8abdb5e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
     ]
     
     override func viewDidLoad() {
@@ -35,8 +41,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath)
         
         if let imageCell = cell as? MyTableViewCell {
-            imageCell.backgroundColor = .yellow
-            return imageCell
+            imageCell.imageURL = URL(string: imageURLs[indexPath.row])
         }
         
         return cell
