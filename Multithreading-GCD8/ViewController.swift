c4938f4d44933dd8cd3ebed7a4b8ebe5e29b6dc2
Rus <de.mr44@gmail.com>
2021-11-28 23:36:56 +0500

    Some code refactoring

--- Multithreading-GCD8/ViewController.swift
+++ Multithreading-GCD8/ViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var myTableView: UITableView!
-    let imageURLs: [String] = [
+    private let imageURLs: [String] = [
         "https://images.unsplash.com/photo-1519181245277-cffeb31da2e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
         "https://images.unsplash.com/photo-1536147116438-62679a5e01f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
         "https://images.unsplash.com/photo-1468971050039-be99497410af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3132&q=80",
@@ -26,12 +26,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        myTableView.delegate = self
         myTableView.dataSource = self
     }
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return imageURLs.count
